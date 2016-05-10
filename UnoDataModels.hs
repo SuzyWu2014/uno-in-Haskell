d82e5d4b5cc52fabd80f465e558f9d47b49ca407
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2016-05-09 22:04:17 -0700

    modify data models

--- UnoDataModels.hs
+++ UnoDataModels.hs
@@ -1,35 +1,57 @@
 module UnoDataModels where
 
-data Card = Skip Color
-          | DrawTwo Color
-          | Reverse Color
-          | Wild
-          | WildDrawFour
-          | Regular Color Int 
+-- data Card = Skip Color
+--           | DrawTwo Color
+--           | Reverse Color
+--           | Wild
+--           | WildDrawFour
+--           | Regular Color Int 
+
+data CardType = Skip  
+              | DrawTwo  
+              | Reverse  
+              | Wild
+              | WildDrawFour
+              | Regular  
+              deriving(Show)
+data Color = Yellow | Red | Blue | Green
+            deriving(Show)
+-- ?? Card effect, what if doesn't need to interacter with user
+-- num - The number in card if there is one.
+-- clr - The color of the card if it's not Wild or WildDrawFour
+-- effect - a function that takes the game state and produce an IO game, which maybe interact with the user to apply the effect of the card
+-- cardType - indicate the type of the card
+-- desc: the description of the card
+data Card = Card { num :: Maybe Int
+                 , clr :: Maybe Color
+                 , effect :: GameState -> IO GameState
+                 , cardType :: CardType
+                 , desc :: String
+}
+instance Show Card where
+  show (Card nm cr _ t d) = case (nm, cr) of
+                  (Just n, Just c)   -> show t ++ "("++show n++", "++show c++"): "++d
+                  (Nothing, Just c)  -> show t ++ " - " ++ show c ++ ": "++d
+                  (Nothing, Nothing) -> show t ++ ": "++ d
+                  _                  -> show d -- ?
 
 data Direction = Clockwise | CounterClockwise
+                deriving(Show)    
 
-data Color = Yellow | Red | Blue | Green
-
-data PlayerState = PlayerState{
+data PlayerState =  PlayerState{
       id :: Int
     , name :: String
-    , numOfCards :: Int
-    , score :: Int
-    , cardsInHand :: [(Int,Card)]
-}
-
-type Deck = [Card]
+    --, score :: Int
+    , cardsInHand :: [(Int, Card)]
+} deriving(Show)
 
 data GameState = GameState {
       dir :: Direction
-    , clr :: Color
-    , num :: Int
     , whoseTurn :: Int
-    , numOfPlayers :: Int
-    , players :: [PlayerState]
     , currCard :: Card
-    , countDeck :: Int
-    , deck :: Deck
-}
+    , players :: [PlayerState] 
+    , deck :: [Card]
+} deriving(Show)
+
+
 
