7bea9474b7fa9421b4b6fbd696a3f3b515733403
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2016-05-09 23:06:21 -0700

    modify get next turn

--- CardEffects.hs
+++ CardEffects.hs
@@ -2,6 +2,8 @@ module CardEffects where
 
 import UnoDataModels
 
+-- Every time when player drops a card, set the GameState.currCard to the droped card, then apply the card effect
+
 -- Card effect - update GameState, including 
     -- current card
     -- each player state
@@ -10,17 +12,19 @@ import UnoDataModels
 
 --Next player in sequence misses a turn
 skip :: GameState -> GameState
-skip (GameState direction currPlayer c playerList d) = GameState direction (getNextTurn (currPlayer + dirt direction) (length playerList)) c playerList d
+skip (GameState _dir _whoseTurn _currCard _players _deck) = GameState _dir (getNextTurn _whoseTurn _players _dir) _currCard _players _deck
 
 type CurrentTurn = Int
 type CountPlayer = Int
 type NextTurn    = Int
+getNextTurn :: CurrentTurn -> [PlayerState]-> Direction -> NextTurn
+getNextTurn _whoseTurn _players _dir = varifyTurnNum (_whoseTurn + dirt _dir) (length _players)
 -- varify the index of current player, making sure it goes in a manner of cycle
-getNextTurn :: CurrentTurn -> CountPlayer-> NextTurn
-getNextTurn curr num_p 
-                       | curr > num_p = 1
-                       | curr == 0    = num_p
-                       | otherwise    = curr 
+varifyTurnNum :: CurrentTurn -> CountPlayer-> NextTurn
+varifyTurnNum _whoseTurn num_p 
+                       | _whoseTurn > num_p = 1
+                       | _whoseTurn == 0    = num_p
+                       | otherwise          = _whoseTurn 
 
 -- turn direction into operation                            
 dirt :: Direction -> Int
