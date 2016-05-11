b9e919879d003143442790a6533d8fc964f5ffd3
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2016-05-10 22:59:28 -0700

    update main function

--- Uno.hs
+++ Uno.hs
@@ -4,3 +4,20 @@ import UnoDataModels
 import GameUtils
 
 --main game process goes here
+simGame :: GameState -> IO GameState
+simGame = undefined
+
+main = do
+       putStrLn "Greetings!  What is your name?"
+       _name <- getLine
+       putStrLn $ "Welcome to UNO, " ++ _name ++ "!"
+       putStrLn "How many players would you like to play with? [1-4]"
+       _num <- getLine
+       putStrLn "Initializing game..."
+       --run game
+       let game = initGame (read _num :: Int)
+       putStrLn "Starting game..."
+       end <- simGame game
+       putStrLn "Game is over."
+
+
