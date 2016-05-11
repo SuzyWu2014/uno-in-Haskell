7f39de8e8cb200d191fb380e10016c9e46a1d230
Yuan Zhang <yuan.vergil@gmail.com>
2016-05-10 22:57:43 -0700

    update

--- GameUtils.hs
+++ GameUtils.hs
@@ -54,8 +54,18 @@ dropCard :: GameState -> GameState
 dropCard = undefined
 
 --draw one card from deck
-drawCard :: GameState -> GameState
-drawCard = undefined
+drawCard :: GameState -> Int -> IO GameState
+drawCard game@GameState{deck=(card:ds), players=_players} usr = return game{deck=ds, players=p}
+  where
+    u@(PlayerState _ _ _ _cardsInHand) = players game !! usr
+    p = take usr _players ++ [u{cardsInHand=card:_cardsInHand}] ++
+        drop (usr+1) _players
+
+drawCards :: Int -> GameState -> Int -> IO GameState
+drawCards 0 game _ = return game
+drawCards n game usr = do
+    game' <- drawCard game usr
+    drawCards (n-1) game' usr
 
 -- IO pick an color and update GameState
 pickColor :: GameState -> GameState
