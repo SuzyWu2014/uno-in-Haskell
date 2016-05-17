b01d6df2c2088720e82ee1d6f3ed358eafad4593
Yuan Zhang <yuan.vergil@gmail.com>
2016-05-17 14:37:19 -0700

    nothing changed

--- Config.hs
+++ Config.hs
@@ -14,7 +14,7 @@ cardPile = let zeros    = [Card 0 c Regular "0" | c <- [Yellow .. Green]] in
            let ncards   = [Card n c Regular d | n <- [1..9], c <- [Yellow .. Green], d <- [show n]] in
            let funcards = [Card 20 c t d | c <- [Yellow .. Green], t <- [Skip .. Reverse], d <- [show t]] in
            let blacks   = [Card 50 PickAColor t d | t <- [Wild, WildDrawFour], d <- [show t]]
-           in zeros ++ (concatMap (replicate 2) (ncards ++ funcards)) ++ concatMap (replicate 4) blacks
+           in zeros ++ concatMap (replicate 2) (ncards ++ funcards) ++ concatMap (replicate 4) blacks
 
 
 -- cardPile :: Deck
